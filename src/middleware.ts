513e0fb02688835acbcb57fa1e5ba5f2a32de51c
Li <1657285609@qq.com>
2024-08-29 21:18:19 +0800

    feat: 去掉主动redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,6 @@ import createMiddleware from 'next-intl/middleware';
 import { locales } from './config';
 
 export default function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  let hadLocale = false;
-
-  for (let i = 0; i < locales.length; i++) {
-    if (pathname.startsWith(`/${locales[i]}`)) {
-      hadLocale = true;
-      break;
-    }
-  }
-
-  if (!hadLocale) {
-    NextResponse.redirect(new URL(`/en${pathname}`, request.url));
-  }
-
   const i18nMiddleware = createMiddleware({
     // A list of all locales that are supported
     locales: locales,
@@ -36,5 +22,5 @@ export default function middleware(request: NextRequest) {
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/(en|es|ru|vi|zh)/:path*'],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/(en)/:path*'],
 };
